73ef90da42ab8ee18d91bd28bc29f60806331af2
ef vl <efvl2022@gmail.com>
2022-11-21 14:39:43 +0100

    add paging for wordCards

--- src/pages/WordCards.jsx
+++ src/pages/WordCards.jsx
@@ -1,4 +1,5 @@
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
+import PaginationBar from "../components/PaginationBar";
 import WCardActionBar from "../features/card/components/WCardActionBar";
 import WordCardTable from "../features/card/components/WordCardTable";
 import WordCardService from "../features/card/services/WordCardService";
@@ -7,25 +8,39 @@ import Layout from "../layout/Layout";
 const WordCards = () => {
 
     const [wcards, setWcards] = useState([]);
-
-    useEffect(() => {
-        fetchWordCards();
-    }, []);
-
-    async function fetchWordCards() {
-        const searchData = {
+    const [currentPage, setCurrentPage] = useState(0);
+    const [totalPages, setTotalPages] = useState(5);
+    const [searchData, setSearchData] = useState(
+        {
             "ids": [ 0 ],
             "language": "string",
-            "word": "string"
-          };
+            "word": "string",
+            "curNumPage": 0,
+            "sizeOfPage": 5
+        }
+    );
+
+    useEffect(() => {
+        fetchWordCards(searchData);
+    }, [searchData]);
+
+    const fetchWordCards = async (searchData) => {
+        console.log(searchData);  
         const response = await WordCardService.searchWordCards(searchData);
         console.log(response.data);
-        setWcards(response.data);
+        setWcards(response.data.content);
+        setCurrentPage(response.data.number);
+        setTotalPages(response.data.totalPages);
     }
+
+    const handleChangePage = useCallback((page) => {
+        console.log(page);
+        setSearchData({...searchData, curNumPage : page}); 
+    }, []);
 
     const deleteWordCard = async (id) => {
         await WordCardService.deleteWordCard(id);
-        fetchWordCards();
+        fetchWordCards(searchData);
     } 
 
     return (
@@ -33,6 +48,7 @@ const WordCards = () => {
         <Layout>
             <WCardActionBar/>
             <WordCardTable wcards={wcards} remove={deleteWordCard}/>
+            <PaginationBar currentPage={currentPage} totalPages={totalPages} onChangePage={handleChangePage}/>
         </Layout>
         </>
     );
