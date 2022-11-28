51e32f99babe56c2ca7d9d38e158877be51edf53
ef vl <efvl2022@gmail.com>
2022-11-28 16:42:35 +0100

    create wordLesson form

--- src/features/lesson/components/AddWLessonPanel.jsx
+++ src/features/lesson/components/AddWLessonPanel.jsx
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { InputGroup } from "react-bootstrap";
 import LangDropdown from "../../card/components/LangDropdown";
 import { useNavigate, useLocation } from 'react-router-dom';
 import WordLessonService from "../services/WordLessonService";
@@ -15,35 +16,120 @@ const AddWLessonPanel = (props) => {
     const location = useLocation();
     const navigate = useNavigate();
 
-    const [wlessonForm, setWLessonForm] = useState({});
+    const [wlessonForm, setWLessonForm] = useState({ name: '', countDone: 1, countChars : 1 });
+    const [fromLangs, setFromLangs] = useState([]);
+    const [fromLabel, setFromLabel] = useState('');
+    const [fromSelectLang, setFromSelectLang] = useState({});
+    const [toLangs, setToLangs] = useState([]);
+    const [toLabel, setToLabel] = useState('');
+    const [toSelectLang, setToSelectLang] = useState({});
+
+    useEffect(() => {
+        loadLanguages();
+    }, []);
+
+    const loadLanguages = async () => {
+        const response = await LangService.searchLanguages();
+        if(response.data?.length > 0){
+            setFromLangs(response.data);
+            setToLangs(response.data); 
+        }
+    }
+
+    const handleFromSelectLanguage = (e) => {
+        let fromSelect = fromLangs.find(item => item.id == e);
+        setFromSelectLang(fromSelect);
+        setFromLabel(fromSelect.fullName);
+    }
+
+    const handleToSelectLanguage = (e) => {
+        let toSelect = toLangs.find(item => item.id == e);
+        setToSelectLang(toSelect);
+        setToLabel(toSelect.fullName);
+    }
 
     const submitWordLesson = async (e) => {
         e.preventDefault();
+        if(isEmpty(fromSelectLang) || isEmpty(toSelectLang)){
+            console.log('from or to lang is null');
+            return false;
+        }
+        if(fromSelectLang.id == toSelectLang.id){
+            console.log('from == to langs');
+            return false;
+        }
+        wlessonForm.fromLanguage = fromSelectLang;
+        wlessonForm.toLanguage = toSelectLang;
         const response = await WordLessonService.addWordLesson(wlessonForm);
-        console.log(response.data)
+        console.log(response.data);
+        navigate('/wlesson');
+    }
+
+    const isEmpty = (obj) => {
+        return Object.keys(obj).length === 0;
     }
 
     return (
         <Container className="mt-3">
             <Row>
                 <Col md={3}></Col>
-                <Col md={6}>
+                <Col md={6} className="border rounded pt-3">
                 <Form>
                     <Form.Group as={Row} className="mb-3" controlId="name">
-                        <Form.Label column sm="2">Name : </Form.Label>
-                        <Col sm="10">
+                        <Form.Label column sm="3">Name : </Form.Label>
+                        <Col sm="9">
                             <Form.Control type="text" 
                                 placeholder="Enter name"
                                 value={wlessonForm.name}
                                 onChange={e => setWLessonForm({...wlessonForm, name: e.target.value})}/>
                         </Col>
                     </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="fromLang">
+                        <Form.Label column sm="3">From language : </Form.Label>
+                        <Col sm="9" className="pt-2">
+                            <InputGroup className="mb-3">
+                                <LangDropdown handler={handleFromSelectLanguage} langs={fromLangs}/>
+                                <Form.Control placeholder="Select language"aria-disabled 
+                                    value={fromLabel}
+                                    onChange={e => setFromLabel(fromSelectLang.fullName)} />
+                            </InputGroup> 
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="toLang">
+                        <Form.Label column sm="3">To language : </Form.Label>
+                        <Col sm="9" className="pt-2">
+                            <InputGroup className="mb-3">
+                                <LangDropdown handler={handleToSelectLanguage} langs={toLangs}/>
+                                <Form.Control placeholder="Select language"aria-disabled 
+                                    value={toLabel}
+                                    onChange={e => setToLabel(toSelectLang.fullName)} />
+                            </InputGroup> 
+                        </Col>
+                    </Form.Group>
                     <Form.Group as={Row} className="mb-3" controlId="isReverse">
-                        <Form.Label column sm="2">Reverse : </Form.Label>
-                        <Col sm="10" className="pt-2">
+                        <Form.Label column sm="3">Reverse : </Form.Label>
+                        <Col sm="9" className="pt-2">
                             <Form.Check type='checkbox' 
                                     value={wlessonForm?.reverse} 
-                                    onChange={e => setWLessonForm({...wlessonForm, reverse: e.target.value})}/>
+                                    onChange={e => setWLessonForm({...wlessonForm, reverse: e.target.checked})}/>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="counDone">
+                        <Form.Label column sm="3">Count done : </Form.Label>
+                        <Col sm="9">
+                            <Form.Control type="text" 
+                                placeholder="Enter count that should be done"
+                                value={wlessonForm.countDone}
+                                onChange={e => setWLessonForm({...wlessonForm, countDone: e.target.value})}/>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="countChars">
+                        <Form.Label column sm="3">Count chars : </Form.Label>
+                        <Col sm="9">
+                            <Form.Control type="text" 
+                                placeholder="Enter minimum count chars that need to check answer"
+                                value={wlessonForm.countChars}
+                                onChange={e => setWLessonForm({...wlessonForm, countChars: e.target.value})}/>
                         </Col>
                     </Form.Group>
                     <div className="text-center p-2">
