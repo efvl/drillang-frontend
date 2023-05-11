3d3c2f885714da66f8102d4ecf0c7ddf3d606f6f
ef vl <efvl2022@gmail.com>
2023-05-11 18:04:05 +0200

    add popover  of examples on manage panel

--- src/features/lesson/components/LessonStatisticPanel.tsx
+++ src/features/lesson/components/LessonStatisticPanel.tsx
@@ -2,9 +2,23 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
 import { Square, XSquare, TrophyFill } from "react-bootstrap-icons";
+import { OverlayTrigger, Popover } from "react-bootstrap";
+import Utils from "../../Utils";
 
 const LessonStatisticPanel = ({trns, lesson}) => {
     console.log(lesson);
+
+    const popover = (str) => {
+        const arr = str.split(/\r?\n/);
+        return arr.length > 0 && arr[0].length > 0
+                ? arr.map((line, index) => <div>{index + 1}. {line}</div>)
+                : <div>no examples</div>
+    } 
+
+    const popoverStyle = {
+        maxWidth: 600,
+        width: 'auto',
+    }
 
     return (
         <Container className="py-2">
@@ -21,10 +35,17 @@ const LessonStatisticPanel = ({trns, lesson}) => {
                 </thead>
                 <tbody>
                     {trns.map((item, index) =>
-                        // <TranslateTableRow key={item.id} rowNum={index + 1} translate={item} remove={remove} />
                         <tr key={item.id}>
                             <td>{item.wlOrder}</td> 
-                            <td>{item.word1}</td>
+                            <OverlayTrigger trigger={['hover', 'focus']} placement="right" overlay={
+                                    <Popover id="popover-basic" style={popoverStyle}>
+                                        <Popover.Body>
+                                            {popover(item.example1)} 
+                                        </Popover.Body>
+                                    </Popover>
+                                }>
+                                <td>{Utils.cutString(item.word1, 100)}</td>
+                            </OverlayTrigger>
                             <td>{item.word2}</td>
                             <td>{item.allAnswer}</td>
                             <td>
